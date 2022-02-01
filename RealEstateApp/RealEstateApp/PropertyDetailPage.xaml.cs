32e49c87fe157698c38a2c816b0563032c26f1cb
Peter Hymøller <pete102c@elevcampus.dk>
2022-02-01 14:42:00 +0100

    working text to speasch

--- RealEstateApp/RealEstateApp/PropertyDetailPage.xaml.cs
+++ RealEstateApp/RealEstateApp/PropertyDetailPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using RealEstateApp.Models;
 using RealEstateApp.Services;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using TinyIoC;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,6 +13,9 @@ namespace RealEstateApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PropertyDetailPage : ContentPage
     {
+
+        public bool PlayBool  { get; set; }
+
         public PropertyDetailPage(PropertyListItem propertyListItem)
         {
             InitializeComponent();
@@ -18,6 +24,8 @@ namespace RealEstateApp
 
             IRepository Repository = TinyIoCContainer.Current.Resolve<IRepository>();
             Agent = Repository.GetAgents().FirstOrDefault(x => x.Id == Property.AgentId);
+
+            PlayBool = true;
 
             BindingContext = this;
         }
@@ -30,5 +38,27 @@ namespace RealEstateApp
         {
             await Navigation.PushAsync(new AddEditPropertyPage(Property));
         }
+
+
+        private async void PlayDiscription_Clicked(object sender, System.EventArgs e)
+        {
+            await SpeakNowDefaultSettings();
+        }
+
+        CancellationTokenSource cts;
+        public async Task SpeakNowDefaultSettings()
+        {
+            cts = new CancellationTokenSource();
+            await TextToSpeech.SpeakAsync(Property.Description, cts.Token);
+        }
+
+        private void CancelSpeech_Clicked(object sender, System.EventArgs e)
+        {
+            if (cts?.IsCancellationRequested ?? true)
+                return;
+
+            cts.Cancel();
+        }
+
     }
 }
