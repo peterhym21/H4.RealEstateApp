f00802eb3e84c0c24afec4e9910eed3244a289df
Peter Hymøller <pete102c@elevcampus.dk>
2022-02-04 10:45:51 +0100

    Added LoginPage

---
+++ RealEstateApp/RealEstateApp/LoginPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RealEstateApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public LoginPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void ButtonLogin_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async Task LoginAsync()
+        {
+
+        }
+
+
+    }
+}
